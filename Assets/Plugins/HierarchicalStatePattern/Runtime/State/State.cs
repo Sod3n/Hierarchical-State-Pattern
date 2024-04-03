a366eb0e082e201af09e5a394a9006b849826011
Nenpotato <59008922+Sod3n@users.noreply.github.com>
2024-04-03 12:27:44 +0700

    add auto disable for states

--- Assets/Plugins/HierarchicalStatePattern/Runtime/State/State.cs
+++ Assets/Plugins/HierarchicalStatePattern/Runtime/State/State.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,9 +8,16 @@ namespace HierarchicalStatePattern
     [RequireComponent(typeof(TransitionManager))]
     public class State : AbstractState
     {
+        private bool _entered = false;
+
+        private void Awake()
+        {
+            if(!_entered) gameObject.SetActive(false);
+        }
 
         public override void Enter()
         {
+            _entered = true;
             gameObject.SetActive(true);
         }
         public override void Exit()
